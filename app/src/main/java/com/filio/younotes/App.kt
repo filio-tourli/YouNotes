1b876184dbb3c9d9917221cae960123f758e3ba1
filio-tourli <filiotourli@gmail.com>
2022-08-19 18:21:26 +0300

    Enable material you dynamic theme

--- app/src/main/java/com/filio/younotes/App.kt
+++ app/src/main/java/com/filio/younotes/App.kt
@@ -1,7 +1,15 @@
 package com.filio.younotes
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
